b4251aca8ad0f1f05098e5509fafb215d19064f9
Maikel Veen <veenmaikel@gmail.com>
2020-06-07 22:48:15 +0200

    Rewrite and build gan network object

--- generator.py
+++ generator.py
@@ -1,53 +1,61 @@
 from tensorflow.keras.layers import *
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
 import helper
 
 class Generator():
-	def __init__(self, noise_shape, res_blocks=16, upsampling=4):
-		self.noise_shape = noise_shape
-		self.upsampling = upsampling
-		self.res_blocks = res_blocks
-		self.generator = self.build_model()
+  """Wrapper around a keras model for a generator cnn network"""
 
-	def get(self):
-		return self.generator
+  def __init__(self, res_blocks=16, upsampling=2):
+    self.upsampling = upsampling
+    self.res_blocks = res_blocks
 
-	def add_res_block(self, model, filters=64, kernel_size=3, strides=1):
-		""" Adds a residual block to the sequential model """
-		model.add(Conv2D(filters = filters, kernel_size = kernel_size, strides = strides, padding = "same"))
-		model.add(BatchNormalization(momentum = 0.5))
-		model.add(PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2]))
-		model.add(Conv2D(filters = filters, kernel_size = kernel_size, strides = strides, padding = "same"))
-		model.add(BatchNormalization(momentum = 0.5))
+    self.generator = self._build_model()
 
-	def add_deconvolution(self, model, filters=256, kernel_size=3, strides=1):
-		"""" Adds a deconvolution 2d block to the sequential model"""
-		model.add(Conv2D(filters = filters, kernel_size = kernel_size, strides = strides, padding = "same"))
-		model.add(UpSampling2D(size = 2))
-		model.add(LeakyReLU(alpha = 0.2))
+  def get_model(self):
+    """Returns the model object"""
+    return self.generator
 
-	def build_model(self):
-		helper.cprint("Building generator model")
-		model = Sequential()
-		model.add(Input(shape=self.noise_shape))
+  def _res_block(self, model, filters=64, kernel_size=3, strides=1):
+    """Appends a residual block to the sequential model """
 
-		# Add pre-residual blocks
-		model.add(Conv2D(filters = 64, kernel_size = 9, strides = 1, padding = "same")) 
-		model.add(PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2]))
+    model = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")(model)
+    model = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1, 2])(model)
+    model = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")(model)
+    return model
 
-		# Add the residual blocks
-		for i in range(self.res_blocks):
-			self.add_res_block(model)
+  def _upsample_block(self, model, filters=256, kernel_size=3, strides=1):
+    """Appends a upsampling 2d block to the sequential model"""
 
-		# Add post residuals block
-		model.add(Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same"))
-		model.add(BatchNormalization(momentum = 0.5))
+    model = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")(model)
+    model = UpSampling2D(size=2)(model)
+    model = LeakyReLU(alpha=0.2)(model)
+    return model
 
-		# Add upsampling blocks
-		for j in range(self.upsampling):
-			self.add_deconvolution(model)
+  def _build_model(self):
+    """Create the model"""
 
-		model.add(Conv2D(filters = 3, kernel_size = 9, strides = 1, padding = "same"))
-		model.add(Activation('tanh'))
-		return model
+    helper.cprint("Building generator network")
+    model_input = Input(shape=(None, None, 3))
 
+    # Add pre-residual blocks
+    model = Conv2D(filters=64, kernel_size=9, strides=1, padding="same")(model_input)
+    model = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(model)
+
+     # Add the residual blocks
+    for _ in range(self.res_blocks):
+      model = self._res_block(model)
+
+    # Add post residuals block
+    model = Conv2D(filters=64, kernel_size=3, strides=1, padding="same")(model)
+    #model = BatchNormalization(momentum = 0.5)(model)
+
+    # Add upsampling blocks
+    for _ in range(self.upsampling):
+      model = self._upsample_block(model)
+
+    model = Conv2D(filters=3, kernel_size=9, strides=1, padding="same")(model)
+    model = Activation('tanh')(model)
+
+    # Create final model oject and name it
+    f_model = Model(inputs=model_input, outputs=model)
+    return f_model
--- network.py
+++ network.py
@@ -1,33 +1,76 @@
-
 import numpy as np
-from generator import Generator
-from discriminator import Discriminator
-from data_loader import DataLoader
+import tensorflow.keras.backend as K
+import helper
 from tensorflow.keras.applications.vgg19 import VGG19
 from tensorflow.keras.models import Model
-import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Input
+from tensorflow.keras.optimizers import Adam
+from data_loader import DataLoader
+from generator import Generator
+from discriminator import Discriminator
 
+def vgg_loss(image_shape, y, y_pred):
+  vgg19 = VGG19(include_top=False, weights='imagenet', input_shape=image_shape)
+  vgg19.trainable = False
+ 
+  for layer in vgg19.layers:
+    layer.trainable = False
+
+  loss_model = Model(inputs=vgg19.input, outputs=vgg19.get_layer('block5_conv4').output)
+  loss_model.trainable = False
+  return K.mean(K.square(loss_model(y) - loss_model(y_pred)))
 
 class SRGAN():
-  def __init__(self):
-    np.random.seed(10)
-    downscale_factor = 4
-    image_shape = (224, 224, 3)
-    shape = (56, 56, 3)
+  """ Class encapsulating the SR GAN network"""
 
-    self.data_loader = DataLoader('data', size=224, downscale_factor=downscale_factor)
+  def __init__(self, verbose=True):
+    np.random.seed(420)
+    self.downscale_factor = 4   
+    self.image_shape = (224, 224, 3)
+    self.shape = (56, 56, 3)
+    self.verbose = verbose
 
-    #gen = Generator((64,64,3))
-    #self.generator = gen.get()
+    # Create data loader object
+    self.data_loader = DataLoader('data', size=224,
+                                  downscale_factor=self.downscale_factor, 
+                                  generate_data=False)
 
-    #dis = Discriminator((64,64,3))
-    #self.discriminator = dis.get()
+    # Create network objects
+    generator = Generator().get_model()
+    discriminator = Discriminator(self.image_shape).get_model()
 
-    # self.generator.summary()
-    # self.discriminator.summary()
+    # Create optimizer and compile networks
+    adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    generator.compile(loss=vgg_loss, optimizer=adam)
+    discriminator.compile(loss="binary_crossentropy", optimizer=adam)
 
-  def get_gan(self, dis, gen, shape, opt):
-    pass
+    # Print model summaries
+    if self.verbose:
+      generator.summary()
+      discriminator.summary()
+    
+    self.network = self.get_gan(discriminator, generator, adam)
+
+  def get_gan(self, discriminator, generator, optimizer):
+    """Returns the full combined network"""
+
+    input_generator_gan = Input(shape=self.shape, name='input_generator_gan')
+    output_generator_gan = generator(input_generator_gan)
+
+    output_discriminator_gan = discriminator(output_generator_gan)
+
+    generator_gan = Model(inputs=input_generator_gan, 
+    outputs=[output_generator_gan, output_discriminator_gan])
+
+    generator_gan.compile(loss=[vgg_loss, "binary_crossentropy"],
+                          loss_weights=[1., 1e-3],
+                          optimizer=optimizer)
+
+    if self.verbose:
+      generator_gan.summary()
+    
+    return generator_gan
+
 
   def train(self, epochs, batch_size):
     pass
@@ -35,14 +78,4 @@ class SRGAN():
   def test(self):
     pass
 
-  def vgg_loss(self, image_shape, y, y_pred):
-    vgg19 = VGG19(include_top=False, weights='imagenet', input_shape=image_shape)
-    vgg19.trainable = False
-
-    for layer in vgg19.layers:
-      layer.trainable = False
-
-    loss_model = Model(inputs=vgg19.input, outputs=vgg19.get_layer('block5_conv4').output)
-    loss_model.trainable = False
-
-    return K.mean(K.square(loss_model(y) - loss_model(y_pred)))
+  
