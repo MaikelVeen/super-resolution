c7ab22856ece41b5ccce225b6175db5f74345536
Pops <pffijt@gmail.com>
2020-06-11 21:34:51 +0200

    Forgot to add import and labels

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 def bprint(text):
 	""" Prints the text in blue """
 	print(f'\033[94m {text} \033[0m')
@@ -19,6 +21,8 @@ def plot_loss(real_losses, fake_losses, gan_losses):
 	ax = fig.add_subplot(1, 2, 1)
 	ax.plot(real_losses,'r-x', label="Loss HR")
 	ax.plot(fake_losses,'b-x', label="Loss LR")
+	ax.xlabel("Epochs")
+	ax.ylabel("Loss")
 	ax.legend()
 	ax.set_title('HR & LR losses')
 	ax.grid(True)
@@ -27,5 +31,7 @@ def plot_loss(real_losses, fake_losses, gan_losses):
 	for i in range(len(gan_losses[0])):
 		ax.plot([pt[i] for pt in gan_losses], label="GAN loss %s"%i )
 	ax.legend()
+	ax.xlabel("Epochs")
+	ax.ylabel("Loss")
 	ax.set_title('Gan losses')
 	ax.grid(True)
