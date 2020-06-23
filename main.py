57ff8f4d6cb5544452d27d4fb003cbfd19114952
Maikel Veen <veenmaikel@gmail.com>
2020-06-23 10:37:31 +0200

    Add data generation to config

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from colorama import init
 from sr_gan import SRGAN
 from dataset_generator import DatasetGenerator
-import helper
-from colorama import init
 
 if __name__ == "__main__":
     init()
     sr_gan = SRGAN()
     sr_gan.compile()
+
+    if sr_gan.config['generate_data'] is True:
+        dataset_generator = DatasetGenerator('data')
+
     if sr_gan.config['model_loading']['active'] is True:
         sr_gan.load_weights(sr_gan.config['model_loading']['epoch'])
-    sr_gan.train()
+        sr_gan.train(sr_gan.config['model_loading']['epoch'])
+    else:
+        sr_gan.train()
